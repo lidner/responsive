3589973fd4f44ce84cb1368b4c88d1f8b70f7e56
lidner <lidner.ls@gmail.com>
2022-04-05 13:42:42 +0600

    Responsive header

--- js/main.js
+++ js/main.js
@@ -5,4 +5,9 @@ $(function () {
 		nextArrow:
 			'<img class="slider__arrow slider__arrow-right" src="images/arrow-right.svg" alt="Arrow Right" />',
 	});
+
+	$('.menu-btn').on('click', function () {
+		$('.menu__list').toggleClass('active');
+		$('.phone').toggleClass('hidden');
+	});
 });
